2bcf8836d7ee04e4028e4fa648cb6b5de9413428
john <jkbrinker@gmail.com>
2018-07-17 21:44:39 -0400

    added looping functionality

--- src/OddEven.java
+++ src/OddEven.java
@@ -6,34 +6,43 @@ public class OddEven {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Hello. What is your name?");
 		String name = scan.nextLine();
-		System.out.println(name + ", enter a number between 1 and 100: ");
-		
-		int userInput = scan.nextInt();
-		if (userInput > 0 && userInput < 101) {
-			if (userInput % 2 == 0) {
-				if (userInput >= 2 && userInput < 25) {
-					System.out.println(name + ", your input is even and less than 25.");
+		boolean cont = true;
+
+		do {
+			System.out.println(name + ", enter a number between 1 and 100: ");
+
+			int userInput = scan.nextInt();
+			if (userInput > 0 && userInput < 101) {
+				if (userInput % 2 == 0) {
+					if (userInput >= 2 && userInput < 25) {
+						System.out.println(name + ", your input is even and less than 25.");
+					} else if (userInput <= 60) {
+						System.out.println(name + ", your input is even.");
+					} else if (userInput > 60) {
+						System.out.println(name + ", " + userInput + " is even.");
+					}
 				}
-				else if (userInput <= 60) {
-					System.out.println(name + ", your input is even.");
-				}
+
 				else if (userInput > 60) {
-					System.out.println(name + ", " + userInput + " is even.");
+					System.out.println(name + ", your input is odd and over 60.");
+				} else {
+					System.out.println(name + ", your input is " + userInput + " and odd.");
 				}
-			}
-			
-			else if (userInput > 60) {
-				System.out.println(name + ", your input is odd and over 60.");
+
 			} else {
-				System.out.println(name + ", your input is odd.");
+				System.out.println("Sorry " + name + ", your input wasn't between 1 and 100. Please try again.");
 			}
-			
-		} else {
-			System.out.println("Sorry " + name + ", your input wasn't between 1 and 100. Please try again.");
-		}
+
+			System.out.println("Continue? (y/n): ");
+			scan.nextLine();
+			if (scan.nextLine().equals("n")) {
+				cont = false;
+				break;
+			}
+
+		} while (cont = true);
 		
+		System.out.println("Bye!");
 		scan.close();
-
 	}
-
 }
